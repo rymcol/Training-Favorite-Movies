7f2e8859004c90a47202abd73513e51e0225e87d
Ryan Collins <ryan@ryanmcollins.com>
2016-04-15 16:59:56 -0400

    Initial Commit

---
+++ Favorite Movies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favorite Movies
+//
+//  Created by Ryan Collins on 4/15/16.
+//  Copyright © 2016 Ryan Collins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
